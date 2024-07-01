3d0777747693db120b26f17249866c17a7d1227a
Harry Minsky <harry.minsky@gmail.com>
2024-07-01 15:59:57 -0400

    Initalize Next.js App
    This commit intializes the next.js application in the repository using
    'npx create-next-app@latest' in the cli. This application was
    bootstrapped with the Typescript, Eslint, Tailwind, and App Router
    options selected. It further removes the boiler plate css, html and
    public assets, and reduces it to a bare minimum.

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "WNYU 89.1 FM",
+  description: "NYU'S ONLY COLLEGE RADIO STATION",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
