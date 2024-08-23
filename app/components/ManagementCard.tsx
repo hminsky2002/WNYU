ff0258f212787070341551c5f7f707a09822775c
ooferini <mintimalspace@gmail.com>
2024-08-23 02:18:38 -0400

    This commit restyles the about page for both mobile and desktop. It fixes the size of images on the page, and changes the desktop layout for management cards to be on the right side of the screen. It also adds management email links to the cards. Closes issues #86 and #87

--- app/components/ManagementCard.tsx
+++ app/components/ManagementCard.tsx
@@ -10,19 +10,21 @@ export function ManagementCard({
   const { name, role, email, picture } = card || {};
 
   return (
-    <div className="py-1">
-      {picture?.asset?._ref ? (
-        <Image
-          className="aspect-square outline outline-offset-[-3px] outline-black"
-          src={urlFor(picture?.asset?._ref).width(300).height(300).url()}
-          width={300}
-          height={300}
-          alt={name || ''}
-        />
-      ) : null}
-      {name ? <p className="text-xl">{name}</p> : null}
-      {role ? <p className="text-lg">{role}</p> : null}
-      {email ? <p className="text-lg">{email}</p> : null}
+    <div className="flex-shrink py-1">
+      <a href={`mailto: ${email}`}>
+        {picture?.asset?._ref ? (
+          <Image
+            className="aspect-square outline outline-offset-[-3px] outline-black"
+            src={urlFor(picture?.asset?._ref).width(300).height(300).url()}
+            width={200}
+            height={200}
+            alt={name || ''}
+          />
+        ) : null}
+        {name ? <p className="text-xl">{name}</p> : null}
+        {role ? <p className="text-lg">{role}</p> : null}
+        {email ? <p className="text-sm">{email}</p> : null}
+      </a>
     </div>
   );
 }
--- app/components/ManagementCardList.tsx
+++ app/components/ManagementCardList.tsx
@@ -7,7 +7,7 @@ export function ManagementCardList({
   cards: MANAGEMENT_CARDS_QUERYResult;
 }) {
   return (
-    <ul className="grid w-full auto-rows-fr grid-cols-2 gap-x-6 gap-y-4 md:flex">
+    <ul className="mr-7 grid w-full auto-rows-fr grid-cols-2 gap-x-6 gap-y-4 md:flex md:flex-row-reverse md:flex-wrap">
       {cards.map((card) => (
         <ManagementCard card={card} key={card.id} />
       ))}
