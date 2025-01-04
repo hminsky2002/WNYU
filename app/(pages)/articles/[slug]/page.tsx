60a0f787bdfa8d7a12b8d35a3eea3aed3a97e578
Harry Minsky <harry.minsky@gmail.com>
2025-01-04 14:40:05 -0500

    update params to fix build

--- app/(pages)/articles/[slug]/page.tsx
+++ app/(pages)/articles/[slug]/page.tsx
@@ -3,13 +3,17 @@ import { ARTICLE_BY_SLUG_QUERY } from '@/sanity/lib/queries';
 import { ARTICLE_QUERYResult } from '@/sanity.types';
 import { Article } from '@/app/components/Article';
 
+type ArticleParams = Promise<{ slug: string }>;
+
 export default async function ArticlePage({
   params,
 }: {
-  params: { slug: string };
+  params: ArticleParams;
 }) {
+  const { slug } = await params;
+
   const article = await sanityFetch<ARTICLE_QUERYResult>({
-    query: ARTICLE_BY_SLUG_QUERY(params.slug),
+    query: ARTICLE_BY_SLUG_QUERY(slug),
     tags: ['article'],
   });
 
