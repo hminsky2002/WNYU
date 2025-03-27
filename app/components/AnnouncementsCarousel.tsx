9c8bc58caf8aaded58ab43430847563c2f38d66d
ooferini <mintimalspace@gmail.com>
2025-03-27 18:47:27 -0400

    add link to announcements

--- app/components/AnnouncementsCarousel.tsx
+++ app/components/AnnouncementsCarousel.tsx
@@ -3,11 +3,11 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
 import Image from 'next/image';
+import Link from 'next/link';
 import { urlFor } from '@/sanity/lib/image';
 import type { ANNOUNCEMENTS_QUERYResult } from '../../sanity.types';
 import 'swiper/css';
 import 'swiper/css/autoplay';
-import Link from 'next/link';
 
 export default function AnnouncementsCarousel({
   announcements,
@@ -25,32 +25,39 @@ export default function AnnouncementsCarousel({
       modules={[Autoplay]}
       className="h-full w-full border-2 border-black"
     >
-      {announcements.map((announcement) =>
-        announcement.link ? (
-          <Link href={announcement.link}>
-            <SwiperSlide key={announcement.subtitle} className="relative">
-              <Image
-                src={urlFor(
-                  announcement?.announcementImage?.asset?._ref ??
-                    '/placeholder.png',
-                )
-                  .width(300)
-                  .height(300)
-                  .url()}
-                fill
-                className="object-cover"
-                alt={announcement.title || ''}
-              />
+      {announcements.map((announcement) => (
+        <SwiperSlide key={announcement.subtitle} className="relative">
+          <Image
+            src={urlFor(
+              announcement?.announcementImage?.asset?._ref ??
+                '/placeholder.png',
+            )
+              .width(300)
+              .height(300)
+              .url()}
+            fill
+            className="object-cover"
+            alt={announcement.title || ''}
+          />
+          {announcement.link ? (
+            <Link href={announcement.link}>
               <div className="absolute bottom-0 w-full bg-black text-white md:h-1/5">
                 <div className="mx-2 my-2 md:mx-4 md:my-4">
                   <h4 className="break-words">{announcement.title}</h4>
                   <p className="ml-1">{announcement.subtitle}</p>
                 </div>
               </div>
-            </SwiperSlide>
-          </Link>
-        ) : null,
-      )}
+            </Link>
+          ) : (
+            <div className="absolute bottom-0 w-full bg-black text-white md:h-1/5">
+              <div className="mx-2 my-2 md:mx-4 md:my-4">
+                <h4 className="break-words">{announcement.title}</h4>
+                <p className="ml-1">{announcement.subtitle}</p>
+              </div>
+            </div>
+          )}
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 }
